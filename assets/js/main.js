ddb6df14e706ef8ef95b46440e40dbe8712703d0
Jordana110 <jordanadauer@gmail.com>
2023-06-29 16:52:08 +0200

    JS_Functions

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+//=============Level 1_2
+function intro2(paramName) {
+      let name = "Supercoder:in";
+      console.log(" Hi " + name + " Mein Name ist " + paramName);
+}
+
+intro2("Jordana");
+
+intro2("Mia");
+intro2("Tia");
+
+//=============Level 1_3
+
+function intro3(name, city, age) {
+      console.log(
+            " Hallo, mein Name ist " +
+                  name +
+                  " Ich bin " +
+                  age +
+                  " Jahre alt. Ich komme aus " +
+                  city
+      );
+}
+
+intro3("Jordana", "Düsseldorf", "28");
+
+//===========Level 1_5
+
+function math(number1, number2) {
+      console.log(number1 + number2);
+}
+math(10, 2);
+math(14, 6);
